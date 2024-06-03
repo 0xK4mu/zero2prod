b3c02f1be38d3f54a6e40bbce01a44595c81a6e8
m4c130d <m4c130d@protonmail.com>
2024-06-03 23:44:47 +0200

    added config crate and configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use tokio::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:0")
+    // Panic si on ne peut pas lire la config
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)
         .await
         .expect("Failed to bind random port");
     //On récupère le port assigné par l'OS
